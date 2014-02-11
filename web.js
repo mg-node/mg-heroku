e879da146297898a116315cba0d9d45fdfdb519c
myounggun <myounggun@gmail.com>
2014-02-11 18:05:23 +0900

    static public upload

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 // web.js
 var express = require("express");
-//var logfmt = require("logfmt");
 var app = express();
-
-//app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
+
+app.use(express.static(__dirname + '/public'));
 
 console.log(process.version);
 
