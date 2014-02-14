edd6d61656e175f52d2db6235f405f8b42ed6d23
myounggun <myounggun@gmail.com>
2014-02-14 14:17:58 +0900

    processing controller refactoring

--- patterns/controllers/processing.js
+++ patterns/controllers/processing.js
@@ -1,33 +1,41 @@
 /**
  * processing contoller
  */
-var basePath = __dirname + '/public/processing';
+//function getFilePath(name) {
+	//return require('path').normalize(__dirname + '/public/processing/' + name + '.html');
+//}
+
 var chapters = [
 	{
 		name: 'rgb',
-		links: basePath + '/rgb.html'
+		link: './rgb.html'
 	},
 	{
 		name: 'rgb channel',
-		links: basePath + '/rgb_channel.html'
+		link: './rgb_channel.html'
 	},
 	{
 		name: 'grayscale',
-		links: basePath + '/grayscale.html'
+		link: './grayscale.html'
 	},
 	{
 		name: 'grayscale faster',
-		links: basePath + '/grayscale_faster.html'
+		link: './grayscale_faster.html'
 	},
 	{
 		name: 'blending',
-		link: basePath + '/blending.html'
+		link: './blending.html'
 	}
 ];
 
+var datas = { 
+	title: 'image processing examples', 
+	chapters: chapters 
+}
+
 exports.index = function(req, res) {
 	res.render('processing/index', { 
-		title: 'image processing exsamples', 
-		chapters: chapters 
+		locals: datas, 
+		cache: false
 	});
 };
