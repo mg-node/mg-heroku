7e7cd441a1499e9a824365780ed146685c63ae1e
myounggun <myounggun@gmail.com>
2014-02-21 20:32:53 +0900

    dasauto show & save impl.

--- patterns/controllers/dasauto.js
+++ patterns/controllers/dasauto.js
@@ -1,10 +1,11 @@
 var ServiceCenter = require('../models/dasauto/servicecenter.js');
+var async = require('async');
 
 exports.index = function(req, res) {
 	ServiceCenter.find({}, function(err, docs) {
 		var datas = {
 				title: 'Service Center List',
-				ascenters: docs
+				servicecenters: docs
 		};
 		
 		res.render('dasauto/index', {
@@ -22,24 +23,44 @@ exports.saveform = function(req, res) {
 };
 
 exports.save = function(req, res) {
-	var item = {
-			uid     : req.body.uid,
-			company : req.body.company,
-			dealer  : req.body.dealer,
-			title   : req.body.title,
-			address : req.body.address,
-			tel     : req.body.tel
-	};
-	
-	var collection = new ServiceCenter(item);
-	
-	collection.save(function(err, data) {
-		if (err) {
-			res.send(err);
-		} else {
-			console.log(data);
-			res.send('added ok');
+	async.waterfall([
+		function(callback) {
+			var cnt = 0;
+			ServiceCenter.find({company: req.body.company}, function(err, docs) {
+				cnt = docs.length + 1;
+			});
+			
+			callback(null, cnt);
+		},
+		function(cnt, callback) {
+			var uid = ('000' + cnt).slice(-3);
+			var item = {
+					uid     : uid,
+					company : req.body.company,
+					dealer  : req.body.dealer,
+					title   : req.body.title,
+					address : req.body.address,
+					tel     : req.body.tel
+			};
+			
+			var collection = new ServiceCenter(item);
+			
+			collection.save(function(err, data) {
+				if (err) {
+					res.send(err);
+				} else {
+					console.log(data);
+					res.send('added ok');
+				}
+			});
+			
+			callback(null, 'done');
 		}
+	], function (err, result) {
+		if (err) {
+			console.log(err);
+		}
+		// result now equals 'done'
 	});
 };
 
@@ -51,6 +72,15 @@ exports.show = function(req, res) {
 			res.send('There is no center with uid of ' + uid);
 		} else {
 			console.log('show = ' + doc);
+			var datas = {
+					title: 'Service Center List',
+					servicecenter: doc
+			};
+			
+			res.render('dasauto/show', {
+				locals: datas,
+				cache: false
+			});
 		}
 	});
 };
